56fa401a11d26fc17a347ff6e4abf035cb4b1f08
abeauquel <beauquel.alexandre@gmail.com>
2018-11-07 09:26:33 -0500

    Feat : Fonction pour récupérer un nouvelle id libre

--- app/src/main/assets/www/action/donnee/ProvinceDAO.js
+++ app/src/main/assets/www/action/donnee/ProvinceDAO.js
@@ -1,10 +1,10 @@
 var ProvinceDAO = function () {
-
+    /** @var Provinces listeProvince*/
     var listeProvince = null;
 
     var initialiser = function () {
         if(!listeProvince){
-            listeProvince = [];
+            listeProvince = new Provinces([]);
 
         }
     }
@@ -14,42 +14,38 @@ var ProvinceDAO = function () {
     }
 
     this.ajouter = function (province) {
-   /*     if(listeProvince.length >0){
-            province.id = listeProvince[listeProvince.length-1].id+1;
+       /* if(listeProvince.getListeProvince().length >0){
+            province.id = listeProvince.getListeProvince()[listeProvince.length-1].id+1;
         }
         else{
             province.id=0;
-        }
-        listeProvince[province.id]= province;
-        localStorage['province'] = JSON.stringify(listeProvince);*/
+        }*/
+       province.id = listeProvince.chercherUnNouvelleId();
+        listeProvince.ajouter(province);
+        localStorage['province'] = JSON.stringify(listeProvince.getListeProvince());
     }
 
     this.lister = function () {
-      /*  if(localStorage['province'])
-            listeProvince = JSON.parse(localStorage['province']);
+      if(localStorage['province'])
+            listeProvince.setListeProvince(JSON.parse(localStorage['province']));
 
-        for (position in listeProvince){
-            var province = new Province(listeProvince[position].id,
-                listeProvince[position].nom,
-                listeProvince[position].population,
-                listeProvince[position].superficie,
-                listeProvince[position].capital);
-            listeProvince[position] = province;
-        }*/
-        var listeProvince = new Provinces([
+        for (position in listeProvince.getListeProvince()){
+            var province = new Province(
+                listeProvince.getListeProvince()[position].id,
+                listeProvince.getListeProvince()[position].nom,
+                listeProvince.getListeProvince()[position].population,
+                listeProvince.getListeProvince()[position].superficie,
+                listeProvince.getListeProvince()[position].capital);
+            listeProvince.getListeProvince()[position] = province;
+        }
+       /* var listeProvince = new Provinces([
             new Province(1, "Québec", 8164361, 1542056, "Québec"),
             new Province(2, "Ontario", 13448494, 1076395, "Toronto"),
             new Province(3, "Alberta", 4067175, 661848, "Edmonton"),
             new Province(4, "Colombie-Britannique", 4648055, 944735, "Victoria"),
 
-        ]);
-       /* var listeProvince = [
-            new Province(1, "Québec", 8164361, 1542056, "Québec"),
-            new Province(2, "Ontario", 13448494, 1076395, "Toronto"),
-            new Province(3, "Alberta", 4067175, 661848, "Edmonton"),
-            new Province(4, "Colombie-Britannique", 4648055, 944735, "Victoria"),
+        ]);*/
 
-        ];*/
         return listeProvince;
     }
 
