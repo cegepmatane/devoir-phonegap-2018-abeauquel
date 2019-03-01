04f4e42d4f6836e735b927089f1c5eea0e523b2c
Unknown <beauquel.alexandre@gmail.com>
2019-03-01 14:03:52 -0500

    feat : modification du modele avec le lien image #1

--- app/src/main/assets/www/action/modele/Province.js
+++ app/src/main/assets/www/action/modele/Province.js
@@ -1,6 +1,7 @@
 var Province = function (id, nom, population, superficie, capital) {
     this.id = id;
     this.nom = nom;
+    this.image = image;
     this.population = population;
     this.superficie = superficie;
     this.capital = capital;
