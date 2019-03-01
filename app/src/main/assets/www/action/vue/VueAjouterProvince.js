60731880ca3d5a804bb34114192d34c62ab3fb09
Unknown <beauquel.alexandre@gmail.com>
2019-03-01 14:14:41 -0500

    feat : recuperation de l'image pour l'ajout

--- app/src/main/assets/www/action/vue/VueAjouterProvince.js
+++ app/src/main/assets/www/action/vue/VueAjouterProvince.js
@@ -13,10 +13,11 @@ let VueAjouterProvince = (function() {
             var enregistrerProvince = function (evenement) {
                 evenement.preventDefault();
                 var nom = document.getElementById("nom").value;
+                var image = document.getElementById("lien-image").value;
                 var capital = document.getElementById("capital").value;
                 var population = document.getElementById("population").value;
                 var superficie = document.getElementById("superficie").value;
-                var province= new Province(null, nom, population, superficie, capital);
+                var province= new Province(null, nom, image, population, superficie, capital);
                 actionEnregisterProvince(province);
             }
         })
